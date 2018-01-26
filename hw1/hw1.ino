402bb8a26684093ce5e5e4396fca3ae1dd022767
Logan Kojiro <logankojiro@Logans-MacBook-Pro.local>
2018-01-26 14:23:56 -0500

    Hw done

--- hw1/hw1.ino
+++ hw1/hw1.ino
@@ -1,4 +1,4 @@
-//Outputs: LED,servo
+//Outputs: LED,Serial feedback
 //Inputs:  Potentiometer, button/switch, photoresistor
 
 
@@ -8,7 +8,7 @@ void setup() {
   pinMode(A0,INPUT);//Photoresistor
   pinMode(A1,INPUT); //potentiometer 
   pinMode(7,INPUT); //switch
-  pinMode(A2,OUTPUT); //servo
+  Serial.begin(9600); //serial comm
 
 }
 
@@ -17,21 +17,45 @@ void loop() {
   int currBrightness = analogRead(A0);
   int threshold = analogRead(A1);
   bool nightMode = digitalRead(7); //change this if use button
+  bool LEDon = false;
 
   if (nightMode){ //darker room = light on
     if ((currBrightness < threshold)){
       digitalWrite(12,HIGH);
+      LEDon = true;
     }
-    else digitalWrite(12,LOW);
+    else {
+      digitalWrite(12,LOW);
+      LEDon = false;
+    }
+    
   }
+  
   else { //brighter room = light on
     if ((currBrightness >= threshold)){
       digitalWrite(12,HIGH);
+      LEDon = true;
     }
-    else digitalWrite(12,LOW);
+    else {
+      digitalWrite(12,LOW);
+      LEDon = false;
+    }
   }
 
-  analogWrite(A2,currBrightness); //Write to servo
+  Serial.print("Room Brightness: ");
+  Serial.print(currBrightness);
+  Serial.print("; Setpoint: ");
+  Serial.print(threshold);
   
+  Serial.print("; Switch is in ");
+  if (nightMode) Serial.print("night mode ");
+  else Serial.print("day mode ");
+
+  Serial.print("and the LED is ");
+  if (LEDon) Serial.print("on");
+  else Serial.print("off");
+  
+  
+
 
 }
--- switch_inclass/switch_inclass.ino
+++ switch_inclass/switch_inclass.ino
@@ -1,11 +1,13 @@
 void setup() {
   pinMode(7,INPUT);
+  pinMode(A0,INPUT);
   Serial.begin(9600);
 
 }
 
 void loop() {
   bool isSwitched = digitalRead(7);
-  Serial.println(isSwitched);
+  int readVal = analogRead(A0);
+  Serial.print("Switch says: %d Pot says: %d\n",isSwitched,readVal);
 
 }
